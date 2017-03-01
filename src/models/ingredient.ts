8ea0005fbd1ce2c27561da77d8785aea1d3e6351
astadipati <astadipati@outlook.com>
2017-03-01 22:37:40 +0700

    Show

--- src/models/ingredient.ts
+++ src/models/ingredient.ts
@@ -1,4 +1,5 @@
 export class Ingredient{
     // public memungkinkan dpat diakses publik
+    // ingat ini modal
     constructor (public name: string, public amount: number){}
 }
--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,20 +1,38 @@
-
 import { Component } from '@angular/core';
 // jangan lupa import NgForm nya
 import { NgForm } from "@angular/forms";
 
-import { ShoppingListService } from './../../services/shopping-list'; //inat ini ada di app.module supaya bisa dipanggil servicenya
+import { ShoppingListService } from './../../services/shopping-list'; //ingat ini ada di app.module supaya bisa dipanggil servicenya
+
+import { Ingredient } from './../../models/ingredient';
 
 @Component({
   selector: 'page-shopping-list',
   templateUrl: 'shopping-list.html'
 })
 export class ShoppingListPage {
-    constructor (private slService: ShoppingListService){}
-     onAddItem(form: NgForm){
-      //  supaya tersedia
-      this.slService.addItem(form.value.ingredientName, form.value.Amount);
-      form.reset();
+  // ini listItems dalam bentuk array dan mengimort model ingredient
+  // pertanyaannya adalah bagaimana ngeload semua data itu? satu jalan yaitu ionviewWillEnter method
+  listItems: Ingredient[];
+
+  constructor(private slService: ShoppingListService) { }
+
+  ionViewWillEnter() {
+    // method ini untuk memastikan saja lalu kita buat satu method untuk tampil data loadItems()
+    this.listItems = this.slService.getItems();
+  }
+
+  onAddItem(form: NgForm) {
+    //  supaya tersedia
+    this.slService.addItem(form.value.ingredientName, form.value.Amount);
+    form.reset();
     //console.log(form);
+    // supaya data tetep tidak hilang kita panggil lagi methodnya
+    this.loadItems();
+  }
+
+  // method ini untuk menampilkan
+  private loadItems(){
+    this.listItems = this.slService.getItems();
   }
 }
